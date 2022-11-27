ecf14175a28de8cb69c44d9a9eb316e79214719d
Conrad Wood <cnw@conradwood.net>
2022-11-27 11:02:02 +0000

    .

--- src/golang.conradwood.net/certmanager/server/certmanager-server.go
+++ src/golang.conradwood.net/certmanager/server/certmanager-server.go
@@ -285,7 +285,7 @@ func isValid(hostname string) bool {
 	if strings.HasSuffix(hostname, "localdomain") {
 		return false
 	}
-	if strings.Contains(strings.ToLower(hostname), "proxy.conradwood") {
+	if strings.Contains(strings.ToLower(hostname), ".proxy.conradwood") {
 		return false
 	}
 	return true
