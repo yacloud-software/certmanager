d5e8c2ec9ed7e1996a50d87226235f6a456f6f71
Conrad Wood <cnw@conradwood.net>
2026-06-20 22:11:14 +0100

    do not bother with spam certificates

---
+++ src/golang.conradwood.net/certmanager/server/filters.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func is_public_spam(name string) bool {
+	if strings.Contains(name, ".proxy.") {
+		return true
+	}
+	if strings.Count(name, "www.") > 1 {
+		return true
+	}
+	if strings.Contains(name, "plesk") {
+		return true
+	}
+	return false
+
+}
+func rewrite_host_name(host string) string {
+	hostname := host
+	if is_public_spam(host) {
+		fmt.Printf("rewritten hostname %s to be exactly '%s'\n", host, hostname)
+		return "proxy.conradwood.net"
+	}
+	if strings.Contains(strings.ToLower(hostname), ".proxy.conradwood") {
+		hostname = "proxy.conradwood.net"
+		fmt.Printf("rewritten hostname %s to be exactly '%s'\n", host, hostname)
+	}
+	return hostname
+
+}
